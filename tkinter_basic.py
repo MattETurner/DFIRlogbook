5dcede094646b57fc25e1c0f66d099a7927e12a7
Matthew Turner <5041820+MattETurner@users.noreply.github.com>
2021-09-01 09:41:24 -0400

    Fixed a reported bug where the clipboard was not cleared. Cleaned up unused functions. Addressed PEP8 compliance issues

--- tkinter_basic.py
+++ tkinter_basic.py
@@ -1,33 +1,39 @@
 import tkinter as tk
-from tkinter.constants import BOTTOM, DISABLED, END, LEFT, NORMAL, TOP
-from tkinter import Tk, RIGHT, BOTH, RAISED
-from tkinter.ttk import Frame, Button, Style
+from tkinter.constants import DISABLED, END, LEFT, NORMAL
+from tkinter import RIGHT, BOTH, RAISED
+from tkinter.ttk import Frame
 from datetime import datetime, timezone
-from typing import Text
 
-def Current_Time():
+
+def current_time():
     date_time = (datetime.now(tz=timezone.utc))
-    return date_time.isoformat()    
+    return date_time.isoformat()
 
-def Copy_Text(text):
-    if(str(text)):
-        text = Current_Time() + " | " + text + "\n"
+
+def copy_text(text):
+    if str(text):
+        text = current_time() + " | " + text + "\n"
         textVar.set(text)
         txt_text.configure(state=NORMAL)
-        txt_text.insert(tk.END,text)
+        txt_text.insert(tk.END, text)
         txt_text.configure(state=DISABLED)
-        ent_entry.delete(0,'end') #clear the entry field
+        ent_entry.delete(0, 'end')  # clear the entry field
 
-def Clear_Text():
+
+def clear_text():
     txt_text.configure(state=NORMAL)
-    txt_text.delete('1.0',END) #clear the text field
+    txt_text.delete('1.0', END)  # clear the text field
     txt_text.configure(state=DISABLED)
 
-def Save_Text(text):
+
+def save_text(text):
+    window.clipboard_clear()
     window.clipboard_append(text)
 
+
 def callback(entry):
-    Copy_Text(ent_entry.get())
+    copy_text(ent_entry.get())
+
 
 window = tk.Tk()
 lbl_label = tk.Label(text="Case Log")
@@ -36,29 +42,29 @@ textVar = tk.StringVar()
 textVar.set("")
 
 ent_entry = tk.Entry(
-    width = "80"
+    width="80"
 )
 txt_text = tk.Text(
-    height = "30",
-    state = DISABLED
+    height="30",
+    state=DISABLED
 )
 
 btn_clear = tk.Button(
-    master = window,
-    text = "Clear",
-    command = lambda: Clear_Text()
+    master=window,
+    text="Clear",
+    command=lambda: clear_text()
 )
 
 btn_main = tk.Button(
-    master = window,
-    text = "Submit",
-    command = lambda: Copy_Text(ent_entry.get())
+    master=window,
+    text="Submit",
+    command=lambda: copy_text(ent_entry.get())
 )
 
 btn_save = tk.Button(
-    master = window,
-    text = "Save to Clipboard",
-    command = lambda: Save_Text(txt_text.get("1.0",END))
+    master=window,
+    text="Save to Clipboard",
+    command=lambda: save_text(txt_text.get("1.0", END))
 )
 
 lbl_label.pack()
@@ -72,6 +78,6 @@ btn_save.pack(side=RIGHT, padx=5, pady=5)
 btn_main.pack(side=RIGHT, padx=5, pady=5)
 
 ent_entry.focus_set()
-window.bind('<Return>',callback)
+window.bind('<Return>', callback)
 
 window.mainloop()
